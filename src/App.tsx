b1b18691c20ec968f1b78a060da78fdd861eefe7
Nivesh Wanninayaka_Niva98 <niveshncwanninayaka@gmail.com>
2023-04-18 15:05:50 +0530

    Handle Button Events

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import "./App.css";
+import { Button } from "./components/Button";
 import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Input } from "./components/Input";
+import { Oscar } from "./components/Oscar";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
 import { Status } from "./components/Status";
@@ -27,10 +31,23 @@ function App() {
 
   return (
     <div className="App">
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button Clicked", event, id);
+        }}
+      />
+
+      <Input value="" handleChange={(event => console.log(event))}/>
+
       {/* <Greet name="Nivesh" messageCount={7} isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList}/> */}
-      <Status status="loading"/>
+      {/* <Status status="loading" />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leanardi Discpario!</Heading>
+      </Oscar>
+      <Greet name="Nivsh" isLoggedIn={false} /> */}
     </div>
   );
 }
